2f0fd0b83b29216fe90705deb7d12134ca271f2c
Thomas Dial <tdial@macbookpro.local>
2018-01-15 22:43:25 -0500

    add MIDI note table to the PIC project

---
+++ midi.X/note_table.h
@@ -0,0 +1,12 @@
+/*
+ * File:   note_table.h
+ * Author: Thomas R. Dial <dialtr@gmail.com>
+ * Date:   15 Jan 2018, 4:41 AM
+ */
+#ifndef NOTE_TABLE_H_INCLUDED_
+#define NOTE_TABLE_H_INCLUDED_
+
+// A table mapping MIDI notes to whole-number freqencies.
+extern const int MIDI_NOTE_FREQUENCY_TABLE[];
+
+#endif
